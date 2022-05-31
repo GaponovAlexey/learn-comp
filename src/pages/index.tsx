7b9630ab6408f708bcdea967a09131dbfbc10d0f
Kekelberi <alexei.gaponov@gmail.com>
2022-05-31 19:10:47 +0300

    save

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,10 +18,27 @@ const Home: NextPage = () => {
         <link rel='icon' href='#!' />
       </Head>
 
-      <h1 className='text-red-900 text-2xl'>start</h1>
-      <h1 className='text-red-900 text-2xl font-mac'>start</h1>
-      <h1 className={s.red}>start</h1>
-      <a href='#!'>link</a>
+      <div className='bg-gradient-to-r from-blue-200 to-blue-500 rounded-md p-8 -m-2'>
+        <h1 className='text-3xl font-bold mb-4 '>subscribe me</h1>
+        <form action='' className='flex text-lg w-2/3'>
+          <input
+            type='text'
+            className='border-2 border-blue-400 rounded-md placeholder-blue-400 px-4 py-2 m-2 w-2/5'
+            placeholder='Your name'
+          />
+          <input
+            type='email'
+            className='border-2 border-blue-400 rounded-md placeholder-blue-400 px-4 py-2 m-2 w-2/5'
+            placeholder='email'
+          />
+          <button
+            className='border-2 border-blue-400 rounded-md bg-blue-400 placeholder-blue-400 px-4 py-2 m-2 w-1/5'
+            type='submit'
+          >
+            subscribe
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
