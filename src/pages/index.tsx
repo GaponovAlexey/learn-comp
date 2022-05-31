1083183a75e11b83eabd21509116f2e38ad0b1a7
Kekelberi <alexei.gaponov@gmail.com>
2022-05-31 16:55:53 +0300

    save

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>test</title>
+        <link rel='icon' href='#!' />
+      </Head>
+
+      <h1>start</h1>
+    </div>
+  )
+}
+
+export default Home
